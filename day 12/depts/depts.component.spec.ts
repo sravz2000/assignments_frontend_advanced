888a07351a41b696a4ef6b11ca594ee6de3efced
sravz2000 <113408992+sravz2000@users.noreply.github.com>
2022-10-19 15:52:31 +0530

    Add files via upload

---
+++ day 12/depts/depts.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeptsComponent } from './depts.component';
+
+describe('DeptsComponent', () => {
+  let component: DeptsComponent;
+  let fixture: ComponentFixture<DeptsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeptsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeptsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
